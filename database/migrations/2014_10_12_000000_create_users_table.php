89aeb3c42a352c175f41bf4bffe680dc6764b47f
Artugam <artguam@gmailc.com>
2018-12-08 14:16:02 +0100

    migrations update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->smallInteger('isAdmin');
+            $table->smallInteger('isAdmin')->default(0);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
