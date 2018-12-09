ced51afa45f14e125382074b559361f21159afbc
jerzyczek <maciag.mateusz@onet.pl>
2018-12-09 22:25:14 +0100

    home view edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <p>Zostałeś zalogowany!</p>
                 </div>
             </div>
         </div>
