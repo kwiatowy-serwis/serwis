2cb08ce942594619ccfd84e3f9ff55ee524d049f
jerzyczek <maciag.mateusz@onet.pl>
2018-12-30 11:51:04 +0100

    frontend change

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -6,6 +6,19 @@
     <h2>Cena</h2>
     <h2>Ilosc sztuk</h2>
 
+    @foreach($flowers as $key=>$flower)
+
+        @if($flower->id == 3)
+
+
+            <p>{{$flower->name}}</p>
+
+
+        @endif
+
+
+    @endforeach
+
 
 
 @endsection
