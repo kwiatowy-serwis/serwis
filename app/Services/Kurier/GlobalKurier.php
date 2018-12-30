448fc36f26b31d5208098e53b009e9a5735d51db
Artugam <artguam@gmailc.com>
2018-12-30 15:26:21 +0100

    kwiaciarnia krakow and kurier connections

--- app/Services/Kurier/GlobalKurier.php
+++ app/Services/Kurier/GlobalKurier.php
@@ -1,31 +1,31 @@
 <?php
 
-namespace App\Services\Kwiaciarnia;
+namespace App\Services\Kurier;
 
 use App\Services\Connections\HttpMethods;
 use App\Services\Kwiaciarnia\Abstracts\ConnectionBase;
-use App\Services\Kwiaciarnia\Interfaces\Kwiaciarnia;
+use App\Services\Kurier\Interfaces\Kurier;
 
 /**
  * Artur Pilch <artur.pilch12@gmail.com>
  */
-class Krakow extends ConnectionBase implements Kwiaciarnia
+class GlobalKurier extends ConnectionBase implements Kurier
 {
-    private $url = 'http://192.168.56.103/kwiaciarnia-krakow/public/api/flower';
+    private $url = 'http://192.168.56.103/kurier/public/api/kurier';
 
 
-    public function pobierzDane ()
+    public function pobierzKurierow ()
     {
         $result = $this->client->request(HttpMethods::GET, $this->url);
         return $this->responseToArray($result);
     }
 
 
-    public function makeOrder ($id, $quantity)
+    public function makeOrder ($id)
     {
         $params = [
             'form_params' => [
-                'id' => $id,
+                'id' => $id
             ]
         ];
         $endpoint = '/order';
---
+++ app/Services/Kurier/Interfaces/Kurier.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Artur
+ * Date: 02.12.2018
+ * Time: 21:39
+ */
+
+namespace App\Services\Kurier\Interfaces;
+
+
+interface Kurier
+{
+    public function pobierzKurierow();
+
+    public function makeOrder($id);
+}
