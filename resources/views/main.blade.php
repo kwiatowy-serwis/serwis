31a6be5c74a95fca9409181795f046bd8d94a249
jerzyczek <maciag.mateusz@onet.pl>
2019-01-02 22:20:40 +0100

    button welcome margin-top

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,7 +10,8 @@
                 <img src="{{$flower->flowerImage}}" alt="roza" width="200px" height="200px">
                 <div class="caption">
                     <h3>{{$flower->name}}</h3>
-                    <p>Count: {{$flower->quantity}}</p>
+                    <p>Ilość: {{$flower->quantity}}</p>
+                    <p>Cena: {{$flower->price}}</p>
                 </div>
                 <p class="button-flower">
                     {{--<a href="{{route('order')}}" class="btn btn-primary" role="button">Kup Teraz</a>--}}
